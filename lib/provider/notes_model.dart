5d8a49f50960708ecfabb2eac70a8d9ae5fedf45
Jedy Matt Tabasco <jedymatt@gmail.com>
2021-11-17 23:23:53 +0800

    Clear contents in NotesModel class because of incorrect attributes

--- lib/provider/notes_model.dart
+++ lib/provider/notes_model.dart
@@ -1,43 +1,4 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
 
-// Project imports:
-import '../models/task_group.dart';
-
-class NotesModel extends ChangeNotifier {
-  late List<TaskGroup> _taskGroups;
-  late int _selectedIndex = 0;
-
-  late TaskGroup _currentTaskGroup;
-
-  NotesModel() {
-    _taskGroups = [
-      TaskGroup(title: 'My Tasks'),
-    ];
-
-    _currentTaskGroup = _taskGroups.first;
-  }
-
-  get taskGroups => _taskGroups;
-
-  get selectedIndex => _selectedIndex;
-
-  void addTaskGroup(TaskGroup taskGroup) {
-    _taskGroups.add(taskGroup);
-    notifyListeners();
-  }
-
-  void removeTaskGroup(TaskGroup taskGroup) {
-    _taskGroups.remove(taskGroup);
-    notifyListeners();
-  }
-
-  void updateTaskGroup(TaskGroup taskGroup) {}
-
-  void updateSelectedIndex(int newIndex) {
-    if (newIndex >= _taskGroups.length) return;
-
-    _selectedIndex = newIndex;
-    notifyListeners();
-  }
-}
+class NotesModel extends ChangeNotifier {}
