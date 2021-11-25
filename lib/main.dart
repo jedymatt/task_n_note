4af81e0e835d33a612516206c1ff9e08c452c5f7
Jedy Matt Tabasco <jedymatt@gmail.com>
2021-11-25 23:08:18 +0800

    moved App class to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,34 @@
-// Flutter imports:
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
 
-// Project imports:
-import 'app.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'provider/tasks_model.dart';
+import 'screens/login_screen.dart';
 
 void main() {
   runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => TasksModel(),
+        )
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          textTheme: GoogleFonts.nunitoTextTheme(),
+        ),
+        home: LoginScreen(),
+        // home: HomeScreen(),
+      ),
+    );
+  }
 }
