d61008e3c1686c61035112e93d2d938a16256390
Jedy Matt Tabasco <jedymatt@gmail.com>
2021-12-05 00:29:03 +0800

    Added screen for adding note

---
+++ lib/screens/add_note_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddNoteScreen extends StatefulWidget {
+  const AddNoteScreen({Key? key}) : super(key: key);
+
+  @override
+  _AddNoteScreenState createState() => _AddNoteScreenState();
+}
+
+class _AddNoteScreenState extends State<AddNoteScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
