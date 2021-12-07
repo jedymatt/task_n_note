ec3472a282199b45e43642898e392ff7ab5e7e77
Jedy Matt Tabasco <jedymatt@gmail.com>
2021-12-08 00:09:08 +0800

    Add auth_service and user_entity

---
+++ lib/models/user_entity.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+
+class UserEntity {
+  String uid;
+  String? displayName;
+  String? photoUrl;
+  UserEntity({
+    required this.uid,
+    this.displayName,
+    this.photoUrl,
+  });
+
+  factory UserEntity.fromJson(String source) =>
+      UserEntity.fromMap(json.decode(source));
+
+  factory UserEntity.fromMap(Map<String, dynamic> map) {
+    return UserEntity(
+      uid: map['uid'],
+      displayName: map['displayName'],
+      photoUrl: map['photoUrl'],
+    );
+  }
+
+  @override
+  int get hashCode => uid.hashCode ^ displayName.hashCode ^ photoUrl.hashCode;
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is UserEntity &&
+        other.uid == uid &&
+        other.displayName == displayName &&
+        other.photoUrl == photoUrl;
+  }
+
+  UserEntity copyWith({
+    String? uid,
+    String? displayName,
+    String? photoUrl,
+  }) {
+    return UserEntity(
+      uid: uid ?? this.uid,
+      displayName: displayName ?? this.displayName,
+      photoUrl: photoUrl ?? this.photoUrl,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': uid,
+      'displayName': displayName,
+      'photoUrl': photoUrl,
+    };
+  }
+
+  @override
+  String toString() =>
+      'User(uid: $uid, displayName: $displayName, photoUrl: $photoUrl)';
+}
---
+++ lib/services/auth_service.dart
@@ -0,0 +1,89 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import '../models/user_entity.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Stream<UserEntity?> get user {
+    return _auth.authStateChanges().map(
+          (user) => (user != null)
+              ? UserEntity(
+                  uid: user.uid,
+                  displayName: user.displayName,
+                  photoUrl: user.photoURL,
+                )
+              : null,
+        );
+  }
+
+  Future<String> createAccount({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email.trim(),
+        password: password.trim(),
+      );
+      return 'success';
+    } on FirebaseAuthException catch (e) {
+      return e.code;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<String> signIn({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: email.trim(),
+        password: password.trim(),
+      );
+      return 'success';
+    } on FirebaseAuthException catch (e) {
+      return e.code;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<String> signOut() async {
+    try {
+      await _auth.signOut();
+      return 'success';
+    } on FirebaseAuthException catch (e) {
+      return e.code;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<String> signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      await FirebaseAuth.instance.signInWithCredential(credential);
+
+      return 'success';
+    } on FirebaseAuthException catch (e) {
+      return e.code;
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
