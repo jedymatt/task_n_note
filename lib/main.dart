f23db72d963f0bdd9c3b548004f206b3e0b167d9
Jedy Matt Tabasco <jedymatt@gmail.com>
2021-12-08 23:21:48 +0800

    Refactor main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'dart:developer';
-
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -11,13 +10,32 @@ import 'package:task_n_note/provider/notes_model.dart';
 import 'package:task_n_note/screens/home_screen.dart';
 import 'package:task_n_note/services/auth_service.dart';
 
+import 'models/note.dart';
 import 'provider/tasks_model.dart';
 import 'screens/login_screen.dart';
+import 'services/database.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const App());
+  await Firebase.initializeApp(
+      // options: DefaultFirebaseOptions.currentPlatform,
+      );
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => TasksModel()),
+        ChangeNotifierProvider(create: (context) => NotesModel()),
+        StreamProvider.value(
+          value: AuthService().user,
+          initialData: null,
+        ),
+        StreamProvider<List<Note>>.value(
+            value: Database().streamNotes(uid: '0d67M2XeqBU7K8BDfDvJUF9FDM03'),
+            initialData: const <Note>[]),
+      ],
+      child: const App(),
+    ),
+  );
 }
 
 class App extends StatefulWidget {
@@ -32,48 +50,37 @@ class _AppState extends State<App> {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => TasksModel()),
-        ChangeNotifierProvider(create: (context) => NotesModel()),
-        StreamProvider.value(
-          value: AuthService().user,
-          initialData: null,
-        ),
-      ],
-      child: MaterialApp(
-        theme: _buildThemeData(context),
-        home: FutureBuilder(
-          future: _initialization,
-          builder: (context, snapshot) {
-            log(snapshot.data.toString());
-            if (snapshot.hasError) {
-              return const Scaffold(
-                body: Center(
-                  child: Text('Unknown error has occured'),
-                ),
-              );
-            }
-
-            // Once complete, show your application
-            if (snapshot.connectionState == ConnectionState.done) {
-              final user = Provider.of<UserEntity?>(context);
-              if (user == null) {
-                return LoginScreen();
-              } else {
-                return HomeScreen();
-              }
-              // return const Root();
-            }
-            // Otherwise, show something whilst waiting for initialization to complete
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: _buildThemeData(context),
+      home: FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
             return const Scaffold(
-              backgroundColor: Colors.blue,
               body: Center(
-                child: CircularProgressIndicator(),
+                child: Text('Unknown error has occured'),
               ),
             );
-          },
-        ),
+          }
+
+          // Once complete, show your application
+          if (snapshot.connectionState == ConnectionState.done) {
+            final user = Provider.of<UserEntity?>(context);
+            if (user == null) {
+              return LoginScreen();
+            } else {
+              return HomeScreen();
+            }
+            // return const Root();
+          }
+          // Otherwise, show something whilst waiting for initialization to complete
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        },
       ),
     );
   }
@@ -81,7 +88,9 @@ class _AppState extends State<App> {
   ThemeData _buildThemeData(context) {
     return ThemeData(
       textTheme: GoogleFonts.nunitoTextTheme(),
+      visualDensity: VisualDensity.adaptivePlatformDensity,
       primarySwatch: Colors.indigo,
+      shadowColor: Colors.indigoAccent,
       appBarTheme: AppBarTheme.of(context).copyWith(
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         foregroundColor: Theme.of(context).primaryColorDark,
