9ac9561fd74223121c0d0a9499b2904646abae6c
Jedy Matt Tabasco <jedymatt@gmail.com>
2021-12-14 11:14:59 +0800

    Added providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:task_n_note/firebase_options.dart';
-import 'package:task_n_note/provider/notes_model.dart';
-import 'package:task_n_note/services/auth_service.dart';
-import 'package:task_n_note/services/todo_lists_service.dart';
 
+import '../firebase_options.dart';
+import '../provider/notes_model.dart';
+import '../provider/todo_list_value_model.dart';
+import '../services/auth_service.dart';
+import '../services/todo_list_service.dart';
+import '../services/todo_service.dart';
 import 'app.dart';
-import 'provider/tasks_model.dart';
-import 'services/notes_service.dart';
+import 'models/user.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,20 +19,21 @@ void main() async {
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => TasksModel()),
-        ChangeNotifierProvider(create: (context) => NotesModel()),
-        Provider(
-          create: (context) => AuthService(),
+        ChangeNotifierProvider(
+          create: (context) => NotesModel(),
         ),
-        StreamProvider.value(
+        StreamProvider<User?>.value(
           value: AuthService().user,
           initialData: null,
         ),
         Provider(
-          create: (context) => NotesService(),
+          create: (context) => TodoListService(),
         ),
         Provider(
-          create: (context) => TodoListsService(),
+          create: (context) => TodoService(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => TodoListValueModel(),
         ),
       ],
       child: const App(),
