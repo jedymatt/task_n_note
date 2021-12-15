6c129a4e34a863a0f65dc31fb713caafdd9fb6f6
Jedy Matt Tabasco <jedymatt@gmail.com>
2021-12-15 14:41:40 +0800

    Removed unused providers

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../firebase_options.dart';
-import '../provider/notes_model.dart';
-import '../provider/todo_list_value_model.dart';
 import '../services/auth_service.dart';
-import '../services/todo_list_service.dart';
-import '../services/todo_service.dart';
 import 'app.dart';
 import 'models/user.dart';
 
@@ -19,21 +15,9 @@ void main() async {
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(
-          create: (context) => NotesModel(),
-        ),
         StreamProvider<User?>.value(
           value: AuthService().user,
           initialData: null,
-        ),
-        Provider(
-          create: (context) => TodoListService(),
-        ),
-        Provider(
-          create: (context) => TodoService(),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => TodoListValueModel(),
         ),
       ],
       child: const App(),
