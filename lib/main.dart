61007576c6acc0086e4b4441f04c8f5192d38879
Jedy Matt Tabasco <jedymatt@gmail.com>
2021-12-15 22:16:13 +0800

    Remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:task_n_note/models/user.dart';
 
 import '../firebase_options.dart';
 import '../services/auth_service.dart';
 import 'app.dart';
-import 'models/user.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
